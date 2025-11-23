d3ab8979e57ddf8510ed26fecdd09fb9103b935d
anafariass <livia.oliveira1.only@gmail.com>
2025-11-23 17:23:08 -0300

    Corrige nome de artista e implementa função para renderizar cards a partir dos dados JSON

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let dados = [];
 
+let cardContainer = document.querySelector(".card-container");
+
+function renderizarCards(dados) {
+   for (let dado of dados) {
+      let article = document.createElement("article");
+      article.classList.add("card");
+      article.innerHTML = `
+      <H2>${dado.nome}</H2>
+      <p>${dado.ano}</p>
+      <p>${dado.descricao}</p>
+      <a href="${dado.link}" target="_blank">Ouça-me</a>`;	
+      cardContainer.appendChild(article);
+   }
+}
+
 async function IniciarBusca() {
    let resposta = await fetch("data.json");
    dados = await resposta.json();
-   console.log(dados);
+   renderizarCards(dados);
 }
 
